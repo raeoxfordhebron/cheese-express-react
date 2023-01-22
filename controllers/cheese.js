69ed1c0a8572b7b7e65e87f6f2f5cdb329f7e3a8
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-01-22 13:45:57 -0500

    add create and show route

--- controllers/cheese.js
+++ controllers/cheese.js
@@ -8,4 +8,18 @@ const catcher = (res) => (error) => res.status(400).json({error})
 // Index Route
 router.get("/", async (req, res) => {
     const cheeses = await Cheese.find({}).catch(catcher(res))
+    res.json(cheeses)
 })
+
+// Create Route
+router.post("/", async (req, res) => {
+    const cheese = await Cheese.create(req.body).catch(catcher(res))
+    res.json(cheese)
+})
+
+// Show Route
+router.get("/", async (req, res) => {
+    const cheese = await Cheese.findById(req.params.id).catch(catcher(res))
+    res.json(cheese)
+})
+
